5ad817f52150a24e80403adedb69d4326e17f186
HYEJIN HELENA PARK <HYEJIN HELENA PARK@HYEJIN>
2022-12-05 10:39:26 +0900

    -리스코프치환원칙

---
+++ oopProject/src/solid/lsp/Aanimal.java
@@ -0,0 +1,25 @@
+package solid.lsp;
+
+abstract class Animal {
+	
+}
+
+interface Speakable {
+	void speak();
+}
+
+class Cat extends Animal implements Speakable{
+	public void speak() {
+	    System.out.println("냐옹");
+	}
+}
+
+class Cow extends Animal implements Speakable{
+	public void speak() {
+	    System.out.println("음머");
+	}
+}
+
+class Fish extends Animal {
+	
+}
